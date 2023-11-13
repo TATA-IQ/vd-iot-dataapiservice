bca5a0ffc98c3c7f3bfb383f0264cc419fed5d28
aditya.singh <aditya.singh@tataiq.com>
2023-11-13 09:27:02 +0000

    Updated get_kafkatopics.py

--- dataapiservice/src/get_kafkatopics.py
+++ dataapiservice/src/get_kafkatopics.py
@@ -5,7 +5,7 @@ class GetKafkaData():
         if camera_group_id is not None and len(camera_group_id)==1:
                 camera_group_id=str(camera_group_id).replace(',','')
         query=f"""
-            SELECT distinct(cm.camera_id), topic_name FROM `tcl-dev`.kafka_topics kt
+            SELECT distinct(cm.camera_id), topic_name FROM kafka_topics kt
 inner join camera_master cm on cm.camera_id=kt.camera_id
 inner join camera_group_list cgl on cgl.camera_id=cm.camera_id and cgl.camera_id=kt.camera_id
 inner join camera_group cg on cg.camera_group_id=cgl.camera_group_id
@@ -13,7 +13,7 @@ inner join schedule_master sm on sm.camera_group_id=cg.camera_group_id
 where sm.is_deleted=0 and cm.is_deleted=0 and cg.is_deleted=0 and cgl.is_deleted=0 and  cg.camera_group_id in {camera_group_id}
         """
         query1=f"""
-            SELECT distinct(cm.camera_id), topic_name FROM `tcl-dev`.kafka_topics kt
+            SELECT distinct(cm.camera_id), topic_name FROM kafka_topics kt
 inner join camera_master cm on cm.camera_id=kt.camera_id
 inner join camera_group_list cgl on cgl.camera_id=cm.camera_id and cgl.camera_id=kt.camera_id
 inner join camera_group cg on cg.camera_group_id=cgl.camera_group_id
